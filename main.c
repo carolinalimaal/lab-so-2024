5f5f999c67de95812982740ad95b87236c4c29e0
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2024-04-12 00:25:10 +0000

    create main.c

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int processNumber;
+
+  // Asks the user how many processes he wants to create
+  printf("How many process you want to create? ");
+  scanf("%d", &processNumber);
+
+  // The number of process have to be positive
+  if (processNumber <= 0) {
+    printf("\nSorry, the number of process have to be positive.\n");
+    return 1;
+  }
+
+  // Create the child process
+  for (int i = 0; i < processNumber - 1; i++) {
+    pid_t pid;
+    pid = fork();
+
+    // Fail to create the child process
+    if (pid < 0) {
+      printf("Fork failed.\n");
+      return 1;
+    }
+      
+    // Runs the Hello World program for each process
+    else if (pid == 0) {
+      printf("Child process PID = %d\n", getpid()); // Prints the ID of the process
+      
+      execl("./helloWorld", "./helloWorld", NULL); // Runs the Hello World program
+      
+      printf("Exec failed.\n");
+      return 0;
+    }
+  }
+  
+  // Parrent process: waits for all child processes to finish
+  for (int i = 0; i < processNumber; i++) {
+    wait(NULL);
+  }
+
+  printf("Parrent process PID = %d\n", getpid());
+  printf("All processes have finished.\n");
+  execl("./helloWorld", "./helloWorld", NULL);
+  
+  return 0;
+}
